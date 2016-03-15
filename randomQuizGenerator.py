18ae0eaa75eb4b3a1b9d59ffd5a2bae78a9504ff
chrispelaez <cpelaez5@mail.valenciacollege.edu>
2016-03-15 13:02:05 -0400

    Update randomQuizGenerator.py

--- randomQuizGenerator.py
+++ randomQuizGenerator.py
@@ -4,6 +4,10 @@
 
 import random, os
 
+# Folders for \quizzes and \answers
+os.makedirs("./answers", exist_ok=True)
+os.makedirs("./quizzes", exist_ok=True)
+
 # The quiz data. Keys are states and values are their capitals.
 capitals = {'Alabama': 'Montgomery', 'Alaska': 'Juneau', 'Arizona': 'Phoenix',
             'Arkansas': 'Little Rock', 'California': 'Sacramento', 'Colorado': 'Denver',
@@ -28,6 +32,29 @@ capitals = {'Alabama': 'Montgomery', 'Alaska': 'Juneau', 'Arizona': 'Phoenix',
             #       1. instead of making 35 quiz versions, you'll only make 5 quiz versions
             #       2. instead of creating quiz and answer files in the current working directory, create a folder titled 'quizzes' and another folder titled 'answers'.
             #       3. place the randomly-generated quizzes in the 'quizzes' directory.
-            #       4. plaec the corresponding answers in the 'answers' directory.
+
             
-            
+for quizNum in range(5):
+    quizFile = open('./quizzes/capitolsquiz%s.txt' % (quizNum + 1), 'w')
+    answerKeyFile = open('./answers/capitolsquiz_answers%s.txt' % (quizNum +1), 'w') # Create 5 quiz files.
+    quizFile.write('Name:\n\nDate:\n\nPeriod:\n\n')
+    quizFile.write((' ' * 20) + 'State Capitals Quiz (Form %s)' % (quizNum + 1))
+    quizFile.write('\n\n')
+    states = list(capitals.keys())
+    random.shuffle(states)
+
+for questionNum in range(50):
+    correctAnswer = capitals[states[questionNum]] # Cylcle through all 50 states.  Creating a question for each.
+    wrongAnswers = list(capitals.values())
+    del wrongAnswers[wrongAnswers.index(correctAnswer)]
+    wrongAnswers = random.sample(wrongAnswers, 3)
+    answerOptions = wrongAnswers + [correctAnswer]
+    random.shuffle(answerOptions)
+
+quizFile.write('%s. What is the capital of %s?\n' % (questionNum + 1, states[questionNum]))
+for i in range(4):
+    quizFile.write('%s. %s\n' % ('ABCD'[i], answerOptions[i]))     # Generate  question / answer options to the quiz file.
+quizFile.write('\n')
+answerKeyFile.write('%s. %s\n' % (questionNum + 1, 'ABCD'[answerOptions.index(correctAnswer)]))
+quizFile.close()
+answerKeyFile.close()
